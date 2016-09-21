fe304ab4e0819d7c7b5c02454d7f87a20ea2be69
jamil_2342 <jamil_2342@a9c667a7-eb0d-46f4-93cb-8f1bcca7edf2>
2016-09-21 07:41:30 +0000

    the website is ready with all json file show.

---
+++ FastMoney/FastMoney/AEMWebsite/Controllers/JsonFileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web;
+
+namespace AEMWebsite.Controllers
+{
+    public class JsonFileController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/newitem.json"));
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
+
--- FastMoney/FastMoney/AEMWebsite/Controllers/ValuesController.cs
+++ FastMoney/FastMoney/AEMWebsite/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
+using FastMoneyClient;
 
 namespace AEMWebsite.Controllers
 {
@@ -13,6 +15,28 @@ namespace AEMWebsite.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+        public string GetAem()
+        {
+            List<Newslist> l = new List<Newslist>();
+
+            Newslist n = new Newslist();
+            Newslist n1 = new Newslist();
+            Newslist n2 = new Newslist();
+            n.headline = "";
+            n._abstract = "";
+            n.image = @"/content/dam/ebase/Nachrichten/2016/07/ONE1607_LAAA_160427_Barthelmeh-194_Kopfbild.jpg";
+
+
+            n1.headline = "";
+            n1._abstract = "";
+            n1.image = "/content/dam/ebase/Nachrichten/2016/07/160629_DiscoverLH-2.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg.image_file.881.494.0%2c39%2c1280%2c757.file";
+
+            n2.headline = "";
+            n2._abstract = "";
+            n2.image = "";
+           
+            return JsonConvert.SerializeObject(l);
         }
 
         // GET api/values/5
